476955c43d07ee4405005232f896d06d527ff020
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-07-19 14:27:55 -0600

    reinit of starter code and gitignore

---
+++ server.js
@@ -0,0 +1,6 @@
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
